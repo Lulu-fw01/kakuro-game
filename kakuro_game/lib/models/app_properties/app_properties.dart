fe9fbcad4763e1673e298c167a15ec31e135c7fd
luka <limarkov@edu.hse.ru>
2022-02-03 17:34:53 +0300

    timerSettings

--- kakuro_game/lib/models/app_properties/app_properties.dart
+++ kakuro_game/lib/models/app_properties/app_properties.dart
@@ -19,6 +19,23 @@ class AppProperties {
   static Color? getBackgroundColor() => _backgroundColor;
 
 
+
+
+  static bool _timer = false;
+
+  /// Timer state.
+  /// 
+  /// Returns true if timer on,
+  /// false if off.
+  static bool isTimerOn() => _timer;
+
+  /// Set timer state.
+  /// 
+  /// True if on, flase if off.
+  static void setTimer(bool state) {
+    _timer = state;
+  } 
+
   static void setProperties() {
     _buttonColor = Colors.black;
     _backgroundColor = Colors.white;
