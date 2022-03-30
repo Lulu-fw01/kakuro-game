5edd4895fffc158bd5faf6a4dc6e4691dbb520cb
luka <limarkov@edu.hse.ru>
2022-03-31 02:01:02 +0300

    zoom

--- kakuro_game/lib/screens/game_screen/game_screen.dart
+++ kakuro_game/lib/screens/game_screen/game_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kakuro_game/models/app_properties/app_properties.dart';
 import 'package:kakuro_game/utilities/field/field.dart';
-import 'package:kakuro_game/widgets/field/cell/empty_cell.dart';
-import 'package:kakuro_game/widgets/field/cell/input_cell.dart';
-import 'package:kakuro_game/widgets/field/cell/value_cell.dart';
 
 
 
@@ -24,12 +21,14 @@ class GameScreen extends StatelessWidget {
       backgroundColor: AppProperties.backgroundColor,
       // Place buttons in the center of screen.
       body: Center(
-        // Arrange buttons in a collumn.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          // TODO add method which returns list of rows filled with sized rows.
-          children: f.getRows(),
-          //children: ,
+        child: InteractiveViewer(
+          minScale: 0.5,
+          maxScale: 1.6,
+          // Arrange rows of buttons in a column.
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: f.getRows(),
+          ),
         ),
       ),
     ));
