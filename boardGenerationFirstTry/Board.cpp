fc59e85c75d588351e631a38e102afc0babd142e
luka <limarkov@edu.hse.ru>
2022-04-21 03:05:20 +0300

    added board class

---
+++ boardGenerationFirstTry/Board.cpp
@@ -0,0 +1,79 @@
+//
+// Created by luka on 21.04.2022.
+//
+
+#include <iostream>
+#include "Board.h"
+
+Board::Board(int height, int width) {
+    m_height = height;
+    m_width = width;
+    m_cells = std::vector<std::vector<EmptyCell *>>(height, std::vector<EmptyCell *>(width, new EmptyCell));
+}
+
+std::vector<std::vector<EmptyCell *>> Board::getCells() {
+    return m_cells;
+}
+
+/**
+ * Height of the board.
+ * */
+int Board::getHeight() const {
+    return m_height;
+}
+
+/**
+ * Width of the board
+ * */
+int Board::getWidth() const {
+    return m_width;
+}
+
+/**
+ * Check if info cell can be placed here.
+ * */
+bool Board::isValid(int row, int column) const {
+    if (row == m_cells.size() - 2 || column == m_cells[0].size() - 2) {
+        return false;
+    }
+
+    int lefter = column - 2;
+    if (lefter >= 0) {
+        int prevL = column - 1;
+        if (m_cells[row][lefter]->getType() == EmptyCell::Type::TYPE_INFO &&
+            m_cells[row][prevL]->getType() == EmptyCell::Type::TYPE_INPUT) {
+            return false;
+        }
+    }
+
+    int higher = row - 2;
+    if (higher >= 0) {
+        int prevH = row - 1;
+        if (m_cells[higher][column]->getType() == EmptyCell::Type::TYPE_INFO &&
+            m_cells[prevH][column]->getType() == EmptyCell::Type::TYPE_INPUT) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void Board::setCell(int row, int column, EmptyCell *cell) {
+    m_cells[row][column] = cell;
+}
+
+/**
+ * Print board in console.
+ * */
+void Board::print(const Board &board) {
+    for (const auto &i: board.m_cells) {
+        for (auto j: i) {
+            if (j->getType() == EmptyCell::Type::TYPE_INFO) {
+                std::cout << "\033[31m"<< j->getCellStr() <<"\033[0m ";
+            } else {
+                std::cout << j->getCellStr() << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
+
+}
---
+++ boardGenerationFirstTry/Board.h
@@ -0,0 +1,29 @@
+//
+// Created by luka on 21.04.2022.
+//
+
+#ifndef BOARDGENERATIONFIRSTTRY_BOARD_H
+#define BOARDGENERATIONFIRSTTRY_BOARD_H
+
+
+#include <vector>
+#include "EmptyCell.h"
+
+class Board {
+public:
+    Board(int height, int width);
+    std::vector<std::vector<EmptyCell *>> getCells();
+    int getHeight() const;
+    int getWidth() const;
+    bool isValid(int row, int column) const;
+    void setCell(int raw, int column, EmptyCell* cell);
+    static void print(const Board &board);
+
+private:
+    std::vector<std::vector<EmptyCell *>> m_cells;
+    int m_height;
+    int m_width;
+};
+
+
+#endif //BOARDGENERATIONFIRSTTRY_BOARD_H
