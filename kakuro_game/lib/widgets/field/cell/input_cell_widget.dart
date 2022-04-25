3de629fd382a594a4d90c5eab7fbe473d91ec40b
luka <limarkov@edu.hse.ru>
2022-04-26 02:55:40 +0300

    making dialog for selecting input cell value

--- kakuro_game/lib/widgets/field/cell/input_cell_widget.dart
+++ kakuro_game/lib/widgets/field/cell/input_cell_widget.dart
@@ -17,14 +17,56 @@ class _InputCellState extends State<InputCellWidget> {
   /// Value in cell that user has entered.
   int get value => _value;
 
-  void _toggleFavorite() {
-    setState(() {
+  void _showDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+            return Dialog(
+              backgroundColor: fourthColor.withOpacity(0.7),
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    TextButton(onPressed: () => {}, child: const Text("1")),
+                    TextButton(onPressed: () => {}, child: const Text("2")),
+                    TextButton(onPressed: () => {}, child: const Text("3"))
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    TextButton(onPressed: () => {}, child: const Text("4")),
+                    TextButton(onPressed: () => {}, child: const Text("5")),
+                    TextButton(onPressed: () => {}, child: const Text("6"))
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    TextButton(onPressed: () => {}, child: const Text("7")),
+                    TextButton(onPressed: () => {}, child: const Text("8")),
+                    TextButton(onPressed: () => {}, child: const Text("9"))
+                  ],
+                ),
+              ],
+            ),
+          );
+        });
+    
+    /*setState(() {
       if (_value == 9) {
         _value = 1;
       } else {
         _value++;
       }
-    });
+    });*/
 }
 
   @override
@@ -33,7 +75,7 @@ class _InputCellState extends State<InputCellWidget> {
         width: 38,
         height: 38,
         child: TextButton(
-          onPressed: _toggleFavorite,
+          onPressed: _showDialog,
           style: ButtonStyle(
             backgroundColor:
                 MaterialStateProperty.all<Color?>(Colors.blue[800]),
@@ -50,3 +92,37 @@ class _InputCellState extends State<InputCellWidget> {
         );
   }
 }
+
+
+
+/*
+
+Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    TextButton(onPressed: () => {}, child: const Text("1")),
+                    TextButton(onPressed: () => {}, child: const Text("2")),
+                    TextButton(onPressed: () => {}, child: const Text("3"))
+                  ],
+                ),
+                Row(
+                  children: [
+                    TextButton(onPressed: () => {}, child: const Text("4")),
+                    TextButton(onPressed: () => {}, child: const Text("5")),
+                    TextButton(onPressed: () => {}, child: const Text("6"))
+                  ],
+                ),
+                Row(
+                  children: [
+                    TextButton(onPressed: () => {}, child: const Text("7")),
+                    TextButton(onPressed: () => {}, child: const Text("8")),
+                    TextButton(onPressed: () => {}, child: const Text("9"))
+                  ],
+                ),
+              ],
+            )
+            */
