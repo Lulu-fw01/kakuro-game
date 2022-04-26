b7d51f0bd5dc01ce882bb17e3e00aea02eca5bae
luka <limarkov@edu.hse.ru>
2022-04-27 01:02:31 +0300

    added stack in game screen, small change in time_card

--- kakuro_game/lib/screens/game_screen/game_screen.dart
+++ kakuro_game/lib/screens/game_screen/game_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:kakuro_game/models/options_icons.dart';
 import 'package:kakuro_game/providers/stopwatch_notifier.dart';
 import 'package:kakuro_game/widgets/options_floating_button/option_button/stopwatch_button.dart';
+import 'package:kakuro_game/widgets/time/time_card.dart';
 import 'package:provider/provider.dart';
 import 'package:kakuro_game/assets/consts.dart';
 import 'package:kakuro_game/utilities/field/field.dart';
@@ -34,48 +35,40 @@ class GameScreen extends StatelessWidget {
 
     // Get notifier about stopwatch visible.
     final notifier = Provider.of<StopwatchNotifier>(context);
-
+    
     return Scaffold(
       // Background color of the screen.
       backgroundColor: backgroundColor,
       // Place buttons in the center of screen.
-      body: Column(
-        //mainAxisAlignment: MainAxisAlignment.start,
+      body: Stack(
+        alignment: Alignment.topCenter,
         children: [
-          const Expanded(
-            flex: 2,
-            child: Padding(
-              padding: EdgeInsets.all(6.0),
-              child: GameStopwatch(),
-            ),
+          Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width,
+              child: Center(child: LayoutBuilder(
+                builder: (context, constraints) {
+                  return SizedBox(
+                    width: constraints.maxWidth,
+                    height: constraints.maxHeight,
+                    child: ClipRect(
+                        child: InteractiveViewer(
+                      minScale: 0.7,
+                      maxScale: 2,
+                      child: Center(
+                          child: Wrap(
+                        direction: Axis.vertical,
+                        spacing: 1.5,
+                        children: f.getRows(),
+                      )),
+                    )),
+                  );
+                },
+              ))),
+          const Padding(
+            padding: EdgeInsets.all(56.0),
+            child: GameStopwatch(),
           ),
-          Expanded(
-              flex: 8,
-              child: Container(
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width,
-                  child: Center(child: LayoutBuilder(
-                    builder: (context, constraints) {
-                      return SizedBox(
-                        width: constraints.maxWidth,
-                        height: constraints.maxHeight,
-                        child: ClipRect(
-                            child: InteractiveViewer(
-                          //boundaryMargin: const EdgeInsets.all(double.infinity),
-                          //constrained: false,
-                          minScale: 0.7,
-                          maxScale: 2,
-                          // Arrange rows of buttons in a column.
-                          child: Center(child: 
-                          Wrap(
-                            direction: Axis.vertical,
-                            spacing: 1.5,
-                            children: f.getRows(),
-                          )),
-                        )),
-                      );
-                    },
-                  ))))
         ],
       ),
       floatingActionButton: OptionsFab(
--- kakuro_game/lib/widgets/time/time_card.dart
+++ kakuro_game/lib/widgets/time/time_card.dart
@@ -3,7 +3,22 @@ import 'package:kakuro_game/assets/consts.dart';
 
 /// Time card that shows number of seconds,
 /// minutes or hours at the moment.
- Widget timeCard({required String time}) => Column(
+Widget timeCard({required String time}) => Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+          color: buttonColor, borderRadius: BorderRadius.circular(20)),
+      child: Text(
+        time,
+        style: const TextStyle(
+            fontWeight: FontWeight.bold, color: fifthColor, fontSize: 14),
+      ),
+    );
+
+
+
+/*
+
+Widget timeCard({required String time}) => Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
@@ -17,8 +32,7 @@ import 'package:kakuro_game/assets/consts.dart';
                 fontWeight: FontWeight.bold, color: fifthColor, fontSize: 14),
           ),
         ),
-        const SizedBox(
-          height: 12,
-        ),
       ],
     );
+
+*/
