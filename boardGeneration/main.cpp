c1d8abfd5d33a6346946a37b0b2a56bc95c6f9c4
luka <limarkov@edu.hse.ru>
2022-05-04 17:51:41 +0300

    nothing special

--- boardGeneration/main.cpp
+++ boardGeneration/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-    auto board = generation::generate(15, 11, generation::DIFFICULTY_MEDIUM);
+    auto board = generation::generate(11, 11, generation::DIFFICULTY_MEDIUM);
     Board::print(board);
     std::cout << std::endl << board.toNativeFormat();
     return 0;
