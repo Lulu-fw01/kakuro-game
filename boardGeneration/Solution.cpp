b8a78a7f26aff072f5fe5396f602a89f4939800d
luka <limarkov@edu.hse.ru>
2022-05-11 22:02:52 +0300

    added file for checking solution function

---
+++ boardGeneration/Solution.cpp
@@ -0,0 +1,9 @@
+//
+// Created by luka on 11.05.2022.
+//
+
+#include "Solution.h"
+
+bool solution::checkSolution(Board &board) {
+
+}
---
+++ boardGeneration/Solution.h
@@ -0,0 +1,16 @@
+//
+// Created by luka on 11.05.2022.
+//
+
+#ifndef BOARDGENERATIONFIRSTTRY_SOLUTION_H
+#define BOARDGENERATIONFIRSTTRY_SOLUTION_H
+
+
+#include "Board.h"
+
+namespace solution {
+    bool checkSolution(Board& board);
+};
+
+
+#endif //BOARDGENERATIONFIRSTTRY_SOLUTION_H
