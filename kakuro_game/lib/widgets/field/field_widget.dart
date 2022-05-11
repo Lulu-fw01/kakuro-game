d579dbfb10e8e79245ef93e741b60c5188f8a9fb
luka <limarkov@edu.hse.ru>
2022-05-11 19:02:46 +0300

    InteractiveViever works correct

--- kakuro_game/lib/widgets/field/field_widget.dart
+++ kakuro_game/lib/widgets/field/field_widget.dart
@@ -5,52 +5,45 @@ import 'package:provider/provider.dart';
 
 import '../../providers/field_controller.dart';
 
-class FieldWidget extends StatefulWidget {
+/// Field widget.
+/// This widget contains [InteractiveViewer] filled with kakuro cells.
+class FieldWidget extends StatelessWidget {
   const FieldWidget({Key? key}) : super(key: key);
 
-  @override
-  State<StatefulWidget> createState() => _FieldWidgetState();
-}
-
-class _FieldWidgetState extends State<FieldWidget> {
   @override
   Widget build(BuildContext context) {
     final fieldNotifier = Provider.of<FieldController>(context);
 
-    final fieldWidth = fieldNotifier.field.width * 38.0 + 1.5 * (fieldNotifier.field.width - 1);
-    final fieldHeight = fieldNotifier.field.height * 38.0 + 1.5 * (fieldNotifier.field.height - 1);
+    final fieldWidth = fieldNotifier.field.width * 38.0 +
+        1.5 * (fieldNotifier.field.width - 1);
+    final fieldHeight = fieldNotifier.field.height * 38.0 +
+        1.5 * (fieldNotifier.field.height - 1);
 
-    return Container(
-        alignment: Alignment.center,
-
-        child: Center(child: LayoutBuilder(
-          builder: (context, constraints) {
-            return SizedBox(
-              width: constraints.maxWidth,
-              height:  constraints.maxHeight,
-              child: ClipRect(
-                  child: InteractiveViewer(
-                boundaryMargin: EdgeInsets.only(
-                  left: fieldWidth > constraints.maxWidth ? 5 : 0,
-                  right: fieldWidth > constraints.maxWidth
-                      ? (fieldWidth - constraints.maxWidth + 5)
-                      : 0,
-                  top: fieldHeight > 0.8 * constraints.maxHeight ? 100 : 0,
-                  bottom: fieldHeight > 0.8 * constraints.maxHeight
-                      ? (fieldHeight - 0.8 * constraints.maxHeight + 38)
-                      : 5,
-                ),
-                minScale: 0.3,
-                maxScale: 2,
+    return Center(
+        child: InteractiveViewer(
+            boundaryMargin: EdgeInsets.only(
+              left: fieldWidth > MediaQuery.of(context).size.width ? 5 : 0,
+              right: fieldWidth > MediaQuery.of(context).size.width ? 5 : 0,
+              top: fieldHeight > 0.8 * MediaQuery.of(context).size.height
+                  ? 100
+                  : 0,
+              bottom: fieldHeight > 0.8 * MediaQuery.of(context).size.height
+                  ? (fieldHeight -
+                      0.8 * MediaQuery.of(context).size.height +
+                      38)
+                  : 5,
+            ),
+            constrained: false,
+            minScale: 0.3,
+            maxScale: 2,
+            child: SizedBox(
+                width: max(MediaQuery.of(context).size.width, fieldWidth),
+                height: max(MediaQuery.of(context).size.height, fieldHeight),
                 child: Center(
                     child: Wrap(
                   direction: Axis.vertical,
                   spacing: 1.5,
                   children: fieldNotifier.field.getRows(),
-                )),
-              )),
-            );
-          },
-        )));
+                )))));
   }
 }
