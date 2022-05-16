562a4f5b8aea03c1393143244d2a5e689e4ea631
Barbara Rozhenko <varozhenko@edu.hse.ru>
2022-05-16 04:50:51 +0300

    change

--- boardGeneration/Solution.cpp
+++ boardGeneration/Solution.cpp
@@ -12,10 +12,6 @@ bool checkUsedNumbers(const std::vector<int> &usedNumbers) {
     return true;
 }
 
-void clearVector(std::vector<int> &usedNumbers) {
-    usedNumbers.assign(usedNumbers.size(), 0);
-}
-
 bool solution::checkSolution(Board &board) {
     std::vector<int> usedNumbers = std::vector<int>(9);
     for (int i = 0; i < board.getHeight(); ++i) {
@@ -24,40 +20,40 @@ bool solution::checkSolution(Board &board) {
                 // horizontal
                 // right
                 int horizontalSum = 0, verticalSum = 0;
-                for (int k = j; k < board.getWidth(); ++k) {
+                for (int k = j + 1; k < board.getWidth(); ++k) {
                     // Maybe use k = j + 1 instead?
-                    if (k != j) {
+                    //if (k != j) {
                         if (board.getCellType(i, k) == EmptyCell::Type::TYPE_INPUT) {
                             horizontalSum += std::static_pointer_cast<InputCell>(board.getCells()[i][k])->m_actualValue;
                             usedNumbers[std::static_pointer_cast<InputCell>(board.getCells()[i][k])->m_actualValue -1]++;
                         } else {
                             break;
                         }
-                    }
+                   // }
                 }
                 if ((horizontalSum != std::static_pointer_cast<InfoCell>(board.getCells()[i][j])->m_horizontalSum) ||
                     !checkUsedNumbers(usedNumbers)) {
                     return false;
                 }
-                clearVector(usedNumbers);
+                usedNumbers.assign(usedNumbers.size(), 0);
                 // vertical
                 // down
-                for (int k = i; k < board.getHeight(); ++k) {
+                for (int k = i + 1; k < board.getHeight(); ++k) {
                     // As in previous for.
-                    if (k != i) {
+                    //if (k != i) {
                         if (board.getCellType(k, j) == EmptyCell::Type::TYPE_INPUT) {
                             verticalSum += std::static_pointer_cast<InputCell>(board.getCells()[k][j])->m_actualValue;
                             usedNumbers[std::static_pointer_cast<InputCell>(board.getCells()[k][j])->m_actualValue - 1]++;
                         } else {
                             break;
                         }
-                    }
+                    //}
                 }
                 if ((verticalSum != std::static_pointer_cast<InfoCell>(board.getCells()[i][j])->m_verticalSum) ||
                     !checkUsedNumbers(usedNumbers)) {
                     return false;
                 }
-                clearVector(usedNumbers);
+                usedNumbers.assign(usedNumbers.size(), 0);
             }
         }
     }
